2cef1a377b30555d48fafe1fbe447d0a1b8d94cf
Fishmen <adam_phoz18@hotmail.com>
2022-09-05 11:29:34 +1000

    removing errors

--- src/components/Location.js
+++ src/components/Location.js
@@ -7,14 +7,15 @@ export default function Location() {
             <div className='w-5/6 max-w-xl mx-auto text-white rounded border-2' style={{backgroundColor: 'rgba(0,0,0,0.8)'}}>
                 <div className="w-2/3 mx-auto pt-8 pb-8">
                     <h3 className='text-xl font-bold'>Location</h3>
-                    <a href='https://www.google.com/maps/dir//BossFit+Port+Melbourne,+2%2F250+Bay+St,+Port+Melbourne+VIC+3207,+Australia/@-37.8190369,144.9038309,13z/data=!3m1!5s0x6ad667eb6f4c456f:0x4f3cd039bc1b54cd!4m8!4m7!1m0!1m5!1m1!1s0x6ad667ec7383b9d7:0x8d0723140ab18065!2m2!1d144.9430465!2d-37.8382312' target='_blank'>Bossfit, Level 2, 250 Bay Street, Port Melbourne, Vic, 3027</a>
+                    <a href='https://www.google.com/maps/dir//BossFit+Port+Melbourne,+2%2F250+Bay+St,+Port+Melbourne+VIC+3207,+Australia/@-37.8190369,144.9038309,13z/data=!3m1!5s0x6ad667eb6f4c456f:0x4f3cd039bc1b54cd!4m8!4m7!1m0!1m5!1m1!1s0x6ad667ec7383b9d7:0x8d0723140ab18065!2m2!1d144.9430465!2d-37.8382312' target='_blank'  rel="noreferrer">Bossfit, Level 2, 250 Bay Street, Port Melbourne, Vic, 3027</a>
                     <p>Phone: 0433 811 508</p>
                     <p>Email: fitnessbyfish@hotmail.com</p>
-                    <p>Facebook</p>
-                    <p>Instagram</p>
+                    <a href='https://www.facebook.com/fitnessbyfishpt' target='_blank'  rel="noreferrer">Facebook</a>
+                    <br />
+                    <a href='https://www.instagram.com/a_fisher_08' target='_blank'  rel="noreferrer">Instagram</a>
                 </div>
                 <div className='mx-auto w-fit mb-8'>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3150.942714191354!2d144.94085781582993!3d-37.83822694321757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad667ec7383b9d7%3A0x8d0723140ab18065!2sBossFit%20Port%20Melbourne!5e0!3m2!1sen!2sau!4v1662335876933!5m2!1sen!2sau" width="400" height="300" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe title='googleMap' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3150.942714191354!2d144.94085781582993!3d-37.83822694321757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad667ec7383b9d7%3A0x8d0723140ab18065!2sBossFit%20Port%20Melbourne!5e0!3m2!1sen!2sau!4v1662335876933!5m2!1sen!2sau" width="400" height="300" style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,23 +1,35 @@
 import React from "react";
 import lingin from '../images/lingin2.jpg';
+import yusen from '../images/yusenmock.jpg';
 
 export default function Testimonials() {
     return (
         <div id='testimonials-page' className='block w-screen mt-24 justify-center text-center'>
-            <h3 className='font-bold text-xl'>Heres what our members are saying</h3>
+            <h3 className='font-bold text-2xl mb-8'>Heres what our members are saying</h3>
             <div className='flex-wrap md:flex justify-center w-full'>
-                <div className='max-w-lg mx-auto'>
+                <div className='max-w-lg p-4'>
                     <div className=''>
-                        <img src={lingin} className='h-96 rounded-xl m-4 mx-auto' />
+                        <img src={lingin} alt='client' className='h-96 rounded-xl m-4 mx-auto' />
                     </div>
                     <div className='lg:max-w-2xl m-4 md:max-w-lg max-w-md mx-auto'>
                         <h2 className='font-bold text-xl'>Gina</h2>
                         <p>I am a 75 year old woman who suffers from osteoarthritis. I have started personal training with Adam Fisher about 2 years ago. I have been using PT for the past 18 years. Adam is an EXEPTIONAL trainer. Most trainers are professional and skilled, but Adam has a knack - a talent - an intuitive ability to make the pain go away. Adam plans methodically for the sessions but is flexible with exercising when necessary and monitors every move with hawks-eyes. Although chances are that I will not become an athlete, I have gained strength, flexibility and control and as a result a sense of achievement. I always look forward to my meetings with Adam: he has a sense of humor, he is well spoken, considerate and caring. SESSIONS WITH HIM FLY AWAY TOO FAST.</p>
                     </div>
                 </div>
-                <div className='max-w-lg mx-auto'>
+                <div className='max-w-lg p-4'>
                     <div className=''>
-                        <img src={lingin} className='h-96 rounded-xl m-4 mx-auto' />
+                        <img src={lingin} alt='client' className='h-96 rounded-xl m-4 mx-auto' />
+                    </div>
+                    <div className='lg:max-w-2xl m-4 md:max-w-lg max-w-md mx-auto'>
+                        <h2 className='font-bold text-xl'>Linley</h2>
+                        <p>I'm a 76 year old business owner with varied interests. I started running-for-fun in my forties, and continue to do so. I've have had few injuries, none serious. I've used a personal trainer for at least 15 years (apart from technical expertise, this provides motivation), and I started with Adam some 3 years ago.</p>
+                        <p>Adam provides a considered, structured but varied program to help me attain my goals. Without inducing pain I've improved all personal physical parameters, which of course also helps provide a balanced mental approach.</p>
+                        <p>Our regular sessions are both motivating and invigorating. The benefits gained are, to me, immeasurable. During those times when I cannot train because of work travel requirements, I miss the work-outs and on my return am always impatient to resume.</p>
+                    </div>
+                </div>
+                <div className='max-w-lg p-4'>
+                    <div className=''>
+                        <img src={yusen} alt='client' className='h-96 rounded-xl m-4 mx-auto' />
                     </div>
                     <div className='lg:max-w-2xl m-4 md:max-w-lg max-w-md mx-auto'>
                         <h2 className='font-bold text-xl'>Yusen</h2>
