c2b150ada51d3222422b1d85daf1d018fab29f7a
Fishmen <adam_phoz18@hotmail.com>
2022-09-07 09:37:46 +1000

    justify text

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,8 @@ export default function About () {
             </div>
             <div className='lg:max-w-2xl m-4 md:max-w-lg max-w-md mx-auto'>
                 <h2 className='font-bold text-xl'>Adam Fisher</h2>
-                <p>Adam is the founder of Fitness By Fish Personal Training. He has a strong passion for health, fitness and optimising performance. As an educator and coach Adam has over 7 years experience in the health and fitness industry. Adam has a University degree in Exercise science along with qualifications through FMA Strength and Conditioning, Australian Strength and Conditioning Association, CHEK institute and much more. Adam loves helping people realise what their body is truly capable of when they put the right amount of effort in with the correct knowledge.</p>
-                <p>Adam has been putting his experience into practice on himself over the years to understand what it takes to get the best results out of your training. His interests include Indoor and Outdoor Rock Climbing, Hiking and Trekking etc.....
+                <p className='text-justify'>Adam is the founder of Fitness By Fish Personal Training. He has a strong passion for health, fitness and optimising performance. As an educator and coach Adam has over 7 years experience in the health and fitness industry. Adam has a University degree in Exercise science along with qualifications through FMA Strength and Conditioning, Australian Strength and Conditioning Association, CHEK institute and much more. Adam loves helping people realise what their body is truly capable of when they put the right amount of effort in with the correct knowledge.</p>
+                <p className='text-justify'>Adam has been putting his experience into practice on himself over the years to understand what it takes to get the best results out of your training. His interests include Indoor and Outdoor Rock Climbing, Hiking and Trekking etc.....
                 </p>
             </div>
         </div>
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -13,7 +13,7 @@ export default function Testimonials() {
                     </div>
                     <div className='lg:max-w-2xl m-4 md:max-w-lg max-w-md mx-auto'>
                         <h2 className='font-bold text-xl'>Gina</h2>
-                        <p>I am a 75 year old woman who suffers from osteoarthritis. I have started personal training with Adam Fisher about 2 years ago. I have been using PT for the past 18 years. Adam is an EXEPTIONAL trainer. Most trainers are professional and skilled, but Adam has a knack - a talent - an intuitive ability to make the pain go away. Adam plans methodically for the sessions but is flexible with exercising when necessary and monitors every move with hawks-eyes. Although chances are that I will not become an athlete, I have gained strength, flexibility and control and as a result a sense of achievement. I always look forward to my meetings with Adam: he has a sense of humor, he is well spoken, considerate and caring. SESSIONS WITH HIM FLY AWAY TOO FAST.</p>
+                        <p className='text-justify'>I am a 75 year old woman who suffers from osteoarthritis. I have started personal training with Adam Fisher about 2 years ago. I have been using PT for the past 18 years. Adam is an EXEPTIONAL trainer. Most trainers are professional and skilled, but Adam has a knack - a talent - an intuitive ability to make the pain go away. Adam plans methodically for the sessions but is flexible with exercising when necessary and monitors every move with hawks-eyes. Although chances are that I will not become an athlete, I have gained strength, flexibility and control and as a result a sense of achievement. I always look forward to my meetings with Adam: he has a sense of humor, he is well spoken, considerate and caring. SESSIONS WITH HIM FLY AWAY TOO FAST.</p>
                     </div>
                 </div>
                 <div className='max-w-lg p-4 mx-auto'>
@@ -22,7 +22,7 @@ export default function Testimonials() {
                     </div>
                     <div className='lg:max-w-2xl m-4 md:max-w-lg max-w-md mx-auto'>
                         <h2 className='font-bold text-xl'>Linley</h2>
-                        <p>I'm a 76 year old business owner with varied interests. I started running-for-fun in my forties, and continue to do so. I've have had few injuries, none serious. I've used a personal trainer for at least 15 years (apart from technical expertise, this provides motivation), and I started with Adam some 3 years ago.</p>
+                        <p className='text-justify'>I'm a 76 year old business owner with varied interests. I started running-for-fun in my forties, and continue to do so. I've have had few injuries, none serious. I've used a personal trainer for at least 15 years (apart from technical expertise, this provides motivation), and I started with Adam some 3 years ago.</p>
                         <p>Adam provides a considered, structured but varied program to help me attain my goals. Without inducing pain I've improved all personal physical parameters, which of course also helps provide a balanced mental approach.</p>
                         <p>Our regular sessions are both motivating and invigorating. The benefits gained are, to me, immeasurable. During those times when I cannot train because of work travel requirements, I miss the work-outs and on my return am always impatient to resume.</p>
                     </div>
@@ -33,7 +33,7 @@ export default function Testimonials() {
                     </div>
                     <div className='lg:max-w-2xl m-4 md:max-w-lg max-w-md mx-auto'>
                         <h2 className='font-bold text-xl'>Yusen</h2>
-                        <p>I've been training for 3 years and when I came to Adam I had injured my right shoulder. While training together Adam has helped to fix my shoulder and make me stronger than ever. Training with Adam I have learnt how to train for my body, keep myself injury free, lose bodyfat and build muscle. Since starting with Adam I have taken up bouldering and really noticed how our training sessions have helped me to progress so fast that I'm at the level of people who have been climbing years long than me. Our sessions are fun but hard and there is always a good laugh. </p>
+                        <p className='text-justify'>I've been training for 3 years and when I came to Adam I had injured my right shoulder. While training together Adam has helped to fix my shoulder and make me stronger than ever. Training with Adam I have learnt how to train for my body, keep myself injury free, lose bodyfat and build muscle. Since starting with Adam I have taken up bouldering and really noticed how our training sessions have helped me to progress so fast that I'm at the level of people who have been climbing years long than me. Our sessions are fun but hard and there is always a good laugh. </p>
                     </div>
                 </div>
             </div>
